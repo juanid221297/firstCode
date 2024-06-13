5675ed3b95969819c6fe577092b61b4cf865ee36
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-06-13 12:44:47 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
-import React, { useState } from 'react';
-import Navbar from "./Navbar"; 
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
-
-function App() {
-  const [mode, setMode] = useState('dark');
-  const [alert, setAlert] = useState(null);
-
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type
-    });
-
-    setTimeout(() => {
-      setAlert(null);
-    }, 3000);
-  }
-
-  const toggleMode = () => {
-    if (mode === 'dark') {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
-      document.title = 'Light Mode';
-    } else {
-      setMode('dark');
-      document.body.style.backgroundColor = 'black';
-      showAlert("Dark mode has been enabled", "success");
-      document.title = 'Dark Mode';
-    }
-  }
-
+const Navbar = (props) => {
   return (
-    <>
-      <Navbar title="Here is title" aboutText="About text is here:" mode={mode} toggleMode={toggleMode} />
-      
-      <Alert alert={alert} />
-      <div className="container mb-3">
-        <TextForm heading="Enter the text below:" />
-      </div>
-
-    </>
+    <div className='container'>
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+        <div className="container-fluid">
+          <div className="navbar-brand" >{props.title}</div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <div className="nav-link active" aria-current="page" >Home</div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" >Link</div>
+              </li>
+              <li className="nav-item dropdown">
+                <div className="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </div>
+                <ul className="dropdown-menu">
+                  <li><div className="dropdown-item" >About</div></li>
+                  <li><div className="dropdown-item" >Another action</div></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><div className="dropdown-item" >Something else here</div></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link disabled" aria-disabled="true">Disabled</div>
+              </li>
+            </ul>
+            <div className="div d-flex">
+              <div className='bg-primary rounded mx-2' style={{height:'40px'     }}></div>
+            </div>
+            {/* <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+              <input className="form-check-input" type="radio" onClick={props.toggleMode} role="switch" id="flexSwitchCheckDefault"/>
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+            </div> */}
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
-export default App;
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string,
+  mode: PropTypes.string.isRequired,
+  toggleMode: PropTypes.func.isRequired
+}
+
+export default Navbar;
